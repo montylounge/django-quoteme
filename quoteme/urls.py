2cd4ca1360e5a6b6ecab5f91d167abf204fc6e5b
kevin fricovsky <kevin@montylounge.com>
2009-09-02 08:52:03 -0400

    removed admin reference

--- quoteme/urls.py
+++ quoteme/urls.py
@@ -3,14 +3,9 @@ from django.contrib import admin
 from django.views.generic.simple import direct_to_template
 from django.conf import settings
 
-admin.autodiscover()
-
 urlpatterns = patterns('',
-    url(r'^quotes/(?P<slug>[-\w]+)/$', 'quoteme.views.quote_detail', name='quote_detail'),
-    url(r'^quotes/$', 'quoteme.views.quote_list', name='quote_list'),
-
+    url(r'^(?P<slug>[-\w]+)/$', 'quoteme.views.quote_detail', name='quote_detail'),
+    url(r'^$', 'quoteme.views.quote_list', name='quote_list'),
     url(r'^testimonials/$', 'quoteme.views.testimonial_list', name='testimonial_list'),
-
-    url(r'^$', direct_to_template, {'template':'example_home.html'}),
-    (r'^admin/(.*)', admin.site.root),
+    url(r'^welcome/$', direct_to_template, {'template':'example_home.html'}),
 )
